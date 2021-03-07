d556067e6bb600f737c606b2fef32275e3c964e2
Yuxuan Cui <cuiyuuxuan@gmail.com>
2021-03-07 00:44:04 -0800

    integrated

--- src/app.py
+++ src/app.py
@@ -26,6 +26,79 @@ app = dash.Dash(
 )
 server = app.server
 
+#-------------------------eric data cleaning
+## Allow large data set
+alt.data_transformers.enable('data_server')
+
+# Data Wrangling
+whitewine = pd.read_csv('Data/winequality-white.csv', sep=';')
+redwine = pd.read_csv('Data/winequality-red.csv', sep=';')
+
+whitewine["type"] = "white"
+redwine["type"] = "red"
+
+wine = redwine.append(whitewine)
+
+# Add column for factored quality
+conditions = [
+    wine["quality"] < 6,
+    wine["quality"] == 6,
+    wine["quality"] > 6
+]
+
+values = [0, 1, 2]
+
+wine["quality_factor"] = np.select(conditions, values)
+
+# Correlation Data
+
+# Get correlations for each wine type
+corr_df_white = wine.loc[wine['type'] == 'white'].select_dtypes('number').corr('spearman').stack().reset_index(name='corr')
+corr_df_white["type"] = "white"
+
+corr_df_red = wine.loc[wine['type'] == 'red'].select_dtypes('number').corr('spearman').stack().reset_index(name='corr')
+corr_df_red["type"] = "red"
+
+# Bind them together
+corr_df = corr_df_white.append(corr_df_red)
+corr_df["quality_factor"] = 3 # For all qualities
+
+# Subset by quality and for each and bind
+for i in [0,1,2]:
+    #Create white df at ith quality
+    corr_df_white = wine.loc[(wine['type'] == 'white') & (wine["quality_factor"] == i)].select_dtypes('number').corr('spearman').stack().reset_index(name='corr')
+    corr_df_white["type"] = "white"
+    corr_df_white["quality_factor"] = i
+    #create red df at ith quality
+    corr_df_red = wine.loc[(wine['type'] == 'red') & (wine["quality_factor"] == i)].select_dtypes('number').corr('spearman').stack().reset_index(name='corr')
+    corr_df_red["type"] = "red"
+    corr_df_red["quality_factor"] = i
+    # bind to main df
+    corr_df = corr_df.append(corr_df_red)
+    corr_df = corr_df.append(corr_df_white)
+
+#Remove full correlations on diag
+corr_df.loc[corr_df['corr'] == 1, 'corr'] = 0
+# Add column for absolute corr 
+corr_df['abs'] = corr_df['corr'].abs()
+
+# Get a list of unique column names
+variables = corr_df["level_0"].unique()
+variables = np.delete(variables, np.argwhere(variables == "quality_factor")) #Don't want this as an option in scatterplot
+
+# Matrix plot. I couldn't figure out how to make it work at the bottom without a callback input
+
+
+
+
+
+
+
+
+
+
+#-----------------------------
+
 # Describe the layout/ UI of the app
 app.layout = html.Div(
     [dcc.Location(id="url", refresh=False), html.Div(id="page-content")]
@@ -208,5 +281,8 @@ def plot_histogram(qual, winetype, histvalue):
 
 ################------------------------
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+
+    
+    
+if __name__ == '__main__':
+    app.run_server(debug=False,dev_tools_ui=False,dev_tools_props_check=False)
