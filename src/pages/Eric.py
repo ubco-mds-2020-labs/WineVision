2d7ac66e387685dd68c459156e66c46579ad0387
EBAX1 <ebaxter1@hotmail.ca>
2021-03-07 20:05:12 -0800

    Finished wrangling file and altered my code to account for changed df

--- src/pages/Eric.py
+++ src/pages/Eric.py
@@ -11,61 +11,15 @@ import dash_bootstrap_components as dbc
 # Allow large data set
 alt.data_transformers.enable('data_server')
 
-# Data Wrangling
-whitewine = pd.read_csv('Data/winequality-white.csv', sep=';')
-redwine = pd.read_csv('Data/winequality-red.csv', sep=';')
+# Get data
+wine = pd.read_csv("data/processed/wine_quality.csv")
 
-whitewine["type"] = "white"
-redwine["type"] = "red"
-
-wine = redwine.append(whitewine)
-
-# Add column for factored quality
-conditions = [
-    wine["quality"] < 6,
-    wine["quality"] == 6,
-    wine["quality"] > 6
-]
-
-values = [0, 1, 2]
-
-wine["quality_factor"] = np.select(conditions, values)
-
-# Correlation Data
-
-# Get correlations for each wine type
-corr_df_white = wine.loc[wine['type'] == 'white'].select_dtypes('number').corr('spearman').stack().reset_index(name='corr')
-corr_df_white["type"] = "white"
-
-corr_df_red = wine.loc[wine['type'] == 'red'].select_dtypes('number').corr('spearman').stack().reset_index(name='corr')
-corr_df_red["type"] = "red"
-
-# Bind them together
-corr_df = corr_df_white.append(corr_df_red)
-corr_df["quality_factor"] = 3 # For all qualities
-
-# Subset by quality and for each and bind
-for i in [0,1,2]:
-    #Create white df at ith quality
-    corr_df_white = wine.loc[(wine['type'] == 'white') & (wine["quality_factor"] == i)].select_dtypes('number').corr('spearman').stack().reset_index(name='corr')
-    corr_df_white["type"] = "white"
-    corr_df_white["quality_factor"] = i
-    #create red df at ith quality
-    corr_df_red = wine.loc[(wine['type'] == 'red') & (wine["quality_factor"] == i)].select_dtypes('number').corr('spearman').stack().reset_index(name='corr')
-    corr_df_red["type"] = "red"
-    corr_df_red["quality_factor"] = i
-    # bind to main df
-    corr_df = corr_df.append(corr_df_red)
-    corr_df = corr_df.append(corr_df_white)
-
-#Remove full correlations on diag
-corr_df.loc[corr_df['corr'] == 1, 'corr'] = 0
-# Add column for absolute corr 
-corr_df['abs'] = corr_df['corr'].abs()
+corr_df = pd.read_csv("data/processed/correlation.csv")
 
 # Get a list of unique column names
 variables = corr_df["level_0"].unique()
-variables = np.delete(variables, np.argwhere(variables == "quality_factor")) #Don't want this as an option in scatterplot
+variables = np.delete(variables, np.argwhere(variables == "Quality Factor"))
+variables = np.delete(variables, np.argwhere(variables == "Quality Factor Numeric")) #Don't want this as an option in scatterplot
 
 # Setup app
 
@@ -117,7 +71,7 @@ app.layout = dbc.Container([
             dcc.Dropdown(
                 id = "x-axis",
                 options=[{"label": i, "value": i} for i in variables],
-                value = "alcohol",
+                value = "Alcohol (%)",
                 clearable = False
                 ),
 
@@ -126,7 +80,7 @@ app.layout = dbc.Container([
             dcc.Dropdown(
                 id = "y-axis",
                 options=[{"label": i, "value": i} for i in variables],
-                value = "chlorides",
+                value = "Chlorides (g/dm^3)",
                 clearable = False),
      
         ])
@@ -141,7 +95,7 @@ app.layout = dbc.Container([
     dcc.Dropdown(
             id = "histvalue",
             options=[{"label": i, "value": i} for i in variables],
-            value = "chlorides",
+            value = "Chlorides (g/dm^3)",
             clearable = False)
 
 ])
@@ -154,16 +108,16 @@ app.layout = dbc.Container([
 )
 def plot_matrix(qual, winetype):
     if qual in [0,1,2]:
-        subset = corr_df.loc[(corr_df["quality_factor"] == qual) & (corr_df["type"].isin(winetype))]
+        subset = corr_df.loc[(corr_df["Quality Factor Numeric"] == qual) & (corr_df["Wine"].isin(winetype))]
     else:
-        subset = corr_df.loc[corr_df["type"].isin(winetype)]
+        subset = corr_df.loc[corr_df["Wine"].isin(winetype)]
     chart = alt.Chart(subset,title="Correlation Plot for Numeric Features").mark_square().encode(
         alt.X('level_0', title = None),
         alt.Y('level_1', title = None),
-        color=alt.Color('type', scale=alt.Scale(domain=['red', 'white'],
+        color=alt.Color('Wine', scale=alt.Scale(domain=['red', 'white'],
                 range=['darkred', 'blue'])),
         size='abs',
-        tooltip=["type", "corr"]
+        tooltip=["Wine", "corr"]
     ).configure_title(fontSize=18).properties(height=250, width=250)
     return chart.to_html()
 
@@ -180,18 +134,18 @@ def plot_matrix(qual, winetype):
 def plot_scatter(xcol, ycol, qual, winetype):
     # Subset by quality
     if qual in [0,1,2]:
-        subset = wine.loc[(wine["quality_factor"] == qual) & (wine["type"].isin(winetype))]
+        subset = wine.loc[(wine["Quality Factor Numeric"] == qual) & (wine["Wine"].isin(winetype))]
     else:
-        subset = wine.loc[wine["type"].isin(winetype)]
+        subset = wine.loc[wine["Wine"].isin(winetype)]
     # Subset by wine type (red, white, or both)
 
     chart = alt.Chart(subset).mark_circle(size = 0.25).encode(
     alt.X(xcol, scale = alt.Scale(zero = False)),
     alt.Y(ycol, scale = alt.Scale(zero = False)),
-    alt.Color("type", scale=alt.Scale(domain=['red', 'white'],
+    alt.Color("Wine", scale=alt.Scale(domain=['red', 'white'],
                 range=['darkred', 'blue']))
     )
-    regression = chart.transform_regression(xcol,ycol, groupby = ["type"],
+    regression = chart.transform_regression(xcol,ycol, groupby = ["Wine"],
                                         # By default lines don't go beyond data and are hard to read in this dense dataset
                                        extent = [min(wine[xcol]) - 1, max(wine[xcol]) + 1]).mark_line(size = 5)
     chart = (chart + regression)
@@ -208,14 +162,14 @@ def plot_scatter(xcol, ycol, qual, winetype):
 )
 def plot_histogram(qual, winetype, histvalue):
     if qual in [0,1,2]:
-        subset = wine.loc[(wine["quality_factor"] == qual) & (wine["type"].isin(winetype))]
+        subset = wine.loc[(wine["Quality Factor Numeric"] == qual) & (wine["Wine"].isin(winetype))]
     else:
-        subset = wine.loc[wine["type"].isin(winetype)]
+        subset = wine.loc[wine["Wine"].isin(winetype)]
 
     chart = alt.Chart(subset).mark_bar().encode(
         alt.X(histvalue, type = "quantitative", bin=alt.Bin(maxbins=30)),
         alt.Y("count()"),
-        alt.Color("type", scale=alt.Scale(domain=['red', 'white'],
+        alt.Color("Wine", scale=alt.Scale(domain=['red', 'white'],
                 range=['darkred', 'blue']))
     ).properties(height=300, width=1000)
     return chart.to_html()
